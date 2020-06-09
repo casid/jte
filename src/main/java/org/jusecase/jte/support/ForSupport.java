bb7f2a6280bac5bf44640425fb7ead527670a0ca
casid <andy@mazebert.com>
2020-06-09 20:02:43 +0200

    Revert "Add support for isOdd/isEven"
    This reverts commit 4656d6af

--- src/main/java/org/jusecase/jte/support/ForSupport.java
+++ src/main/java/org/jusecase/jte/support/ForSupport.java
@@ -44,14 +44,6 @@ public class ForSupport<T> implements Iterable<ForSupport<T>> {
         return index;
     }
 
-    public boolean isOdd() {
-        return (index + 1) % 2 == 0;
-    }
-
-    public boolean isEven() {
-        return !isOdd();
-    }
-
     public T get() {
         return item;
     }
--- src/test/java/org/jusecase/jte/support/ForSupportTest.java
+++ src/test/java/org/jusecase/jte/support/ForSupportTest.java
@@ -12,18 +12,12 @@ class ForSupportTest {
         String[] items = {"one", "two", "three"};
 
         for (ForSupport<String> item : ForSupport.of(items)) {
-            result.append("First: ").append(item.isFirst())
-                    .append(", Last: ").append(item.isLast())
-                    .append(", Even: ").append(item.isEven())
-                    .append(", Odd: ").append(item.isOdd())
-                    .append(", Index: ").append(item.getIndex())
-                    .append(", Item: ").append(item.get())
-                    .append('\n');
+            result.append("First: ").append(item.isFirst()).append(", Last: ").append(item.isLast()).append(", Index: ").append(item.getIndex()).append(", Item: ").append(item.get()).append('\n');
         }
 
         assertThat(result.toString()).isEqualTo(
-                "First: true, Last: false, Even: true, Odd: false, Index: 0, Item: one\n" +
-                "First: false, Last: false, Even: false, Odd: true, Index: 1, Item: two\n" +
-                "First: false, Last: true, Even: true, Odd: false, Index: 2, Item: three\n");
+                "First: true, Last: false, Index: 0, Item: one\n" +
+                "First: false, Last: false, Index: 1, Item: two\n" +
+                "First: false, Last: true, Index: 2, Item: three\n");
     }
 }

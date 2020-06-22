357e498f2e5491bbb00b01838ae3883b06d8b468
casid <andy@mazebert.com>
2020-06-22 06:15:13 +0200

    FileOutput writes files in UTF-8 encoding

--- src/main/java/org/jusecase/jte/output/FileOutput.java
+++ src/main/java/org/jusecase/jte/output/FileOutput.java
@@ -6,6 +6,7 @@ import java.io.BufferedWriter;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
@@ -16,7 +17,7 @@ public class FileOutput implements TemplateOutput, Closeable {
 
     public FileOutput(Path file) throws IOException {
         Files.createDirectories(file.getParent());
-        writer = Files.newBufferedWriter(file, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
+        writer = Files.newBufferedWriter(file, StandardCharsets.UTF_8, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
     }
 
     @Override

3658b8f07858a9163c8967d1ea02c15b326b0d6d
casid <andy@mazebert.com>
2020-07-04 07:03:59 +0200

    Update javadoc

--- src/main/java/org/jusecase/jte/TemplateMode.java
+++ src/main/java/org/jusecase/jte/TemplateMode.java
@@ -5,6 +5,7 @@ public enum TemplateMode {
      * All templates are precompiled to Java class files.
      * The template engine will load them from the specified location.
      * No JDK is required.
+     * All templates share one class loader with each other.
      * This is recommended when running templates in production.
      * More info: https://github.com/casid/jte/blob/master/DOCUMENTATION.md#precompiling-templates
      */
@@ -13,6 +14,7 @@ public enum TemplateMode {
     /**
      * All templates are compiled to Java class files on demand.
      * A JDK is required.
+     * Every template has its own class loader.
      * This is recommended when running templates on your developer machine.
      */
     Dynamic,

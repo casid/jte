bc21136a5d5529c469db7ba9d8834d4fa7093c46
casid <andy@mazebert.com>
2020-07-19 09:11:14 +0200

    Optimize TemplateUtils.toMap

--- jte-runtime/src/main/java/org/jusecase/jte/internal/TemplateUtils.java
+++ jte-runtime/src/main/java/org/jusecase/jte/internal/TemplateUtils.java
@@ -1,12 +1,17 @@
 package org.jusecase.jte.internal;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+@SuppressWarnings("unused") // by template code
 public class TemplateUtils {
-    @SuppressWarnings("unused") // by template code
     public static Map<String, Object> toMap(Object ... pairs) {
-        Map<String, Object> map = new HashMap<>();
+        if (pairs.length == 0) {
+            return Collections.emptyMap();
+        }
+
+        Map<String, Object> map = new HashMap<>(pairs.length / 2);
         for (int i = 0; i < pairs.length; i += 2) {
             map.put((String)pairs[i], pairs[i + 1]);
         }

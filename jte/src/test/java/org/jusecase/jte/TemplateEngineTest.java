8d66bf525bcf8df11fb444eb373d60dde19598f8
casid <andy@mazebert.com>
2020-07-24 09:24:08 +0200

    Fix syntax error

--- jte/src/test/java/org/jusecase/jte/TemplateEngineTest.java
+++ jte/src/test/java/org/jusecase/jte/TemplateEngineTest.java
@@ -227,7 +227,7 @@ public class TemplateEngineTest {
     void tag_content() {
         givenTag("card", "@param org.jusecase.jte.Content content\n" +
                 "<span>${content}</span>");
-        givenTemplate("@tag.card(@{<b>${model.hello}</b>}, That was a tag!");
+        givenTemplate("@tag.card(@{<b>${model.hello}</b>}), That was a tag!");
         thenOutputIs("<span><b>Hello</b></span>, That was a tag!");
     }
 

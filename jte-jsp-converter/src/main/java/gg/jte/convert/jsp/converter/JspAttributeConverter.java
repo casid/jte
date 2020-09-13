7b3a10ec15adfda3b664cd86044ee6b7867f742c
Andreas Hager <andreas@chrono24.com>
2020-09-13 08:22:35 +0200

    Convert numbers to primitive types

--- jte-jsp-converter/src/main/java/gg/jte/convert/jsp/converter/JspAttributeConverter.java
+++ jte-jsp-converter/src/main/java/gg/jte/convert/jsp/converter/JspAttributeConverter.java
@@ -27,6 +27,18 @@ public class JspAttributeConverter extends AbstractJspDirectiveConverter {
             type = "Object";
         } else if (type.equals("java.lang.Boolean")) {
             type = "boolean";
+        } else if (type.equals("java.lang.Byte")) {
+            type = "byte";
+        } else if (type.equals("java.lang.Short")) {
+            type = "short";
+        } else if (type.equals("java.lang.Integer")) {
+            type = "int";
+        } else if (type.equals("java.lang.Long")) {
+            type = "long";
+        } else if (type.equals("java.lang.Float")) {
+            type = "float";
+        } else if (type.equals("java.lang.Double")) {
+            type = "double";
         } else if (type.startsWith("java.lang.")) {
             type = type.substring("java.lang.".length());
         } else {

ff45176aff0b69453f522f3dc814ae05e11013c3
Andreas Hager <andy@mazebert.com>
2020-09-16 07:38:57 +0200

    Cleanup warning

--- jte-runtime/src/main/java/gg/jte/html/HtmlContent.java
+++ jte-runtime/src/main/java/gg/jte/html/HtmlContent.java
@@ -3,6 +3,7 @@ package gg.jte.html;
 import gg.jte.TemplateOutput;
 import gg.jte.Content;
 
+@SuppressWarnings("unused") // By template code with ContentType.Html
 public interface HtmlContent extends Content {
     void writeTo(HtmlTemplateOutput output);
 

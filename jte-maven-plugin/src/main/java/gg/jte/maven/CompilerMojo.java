8db951cef3174dc85b88b5d6e5894bc618ca6e71
casid <andy@mazebert.com>
2020-10-14 22:13:16 +0200

    Custom HTML policy can be defined in CompilerMojo

--- jte-maven-plugin/src/main/java/gg/jte/maven/CompilerMojo.java
+++ jte-maven-plugin/src/main/java/gg/jte/maven/CompilerMojo.java
@@ -2,11 +2,16 @@ package gg.jte.maven;
 
 import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
+import gg.jte.html.HtmlPolicy;
 import gg.jte.resolve.DirectoryCodeResolver;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -41,6 +46,9 @@ public class CompilerMojo extends AbstractMojo {
     @Parameter(readonly = true)
     public String[] htmlAttributes;
 
+    @Parameter(readonly = true)
+    public String htmlPolicyClass;
+
 
     @Override
     public void execute() {
@@ -55,6 +63,9 @@ public class CompilerMojo extends AbstractMojo {
         TemplateEngine templateEngine = TemplateEngine.create(new DirectoryCodeResolver(source), target, ContentType.valueOf(contentType));
         templateEngine.setHtmlTags(htmlTags);
         templateEngine.setHtmlAttributes(htmlAttributes);
+        if (htmlPolicyClass != null) {
+            templateEngine.setHtmlPolicy(createHtmlPolicy(htmlPolicyClass));
+        }
 
         int amount;
         try {
@@ -70,5 +81,24 @@ public class CompilerMojo extends AbstractMojo {
         long end = System.nanoTime();
         long duration = TimeUnit.NANOSECONDS.toSeconds(end - start);
         getLog().info("Successfully precompiled " + amount + " jte file" + (amount == 1 ? "" : "s") + " in " + duration + "s to " + target);
+    }
+
+    private HtmlPolicy createHtmlPolicy(String htmlPolicyClass) {
+        try {
+            URLClassLoader projectClassLoader = createProjectClassLoader();
+            Class<?> clazz = projectClassLoader.loadClass(htmlPolicyClass);
+            return (HtmlPolicy) clazz.getConstructor().newInstance();
+        } catch (Exception e) {
+            throw new IllegalStateException("Failed to instantiate custom HtmlPolicy " + htmlPolicyClass, e);
+        }
+    }
+
+    private URLClassLoader createProjectClassLoader() throws IOException {
+        URL[] runtimeUrls = new URL[compilePath.size()];
+        for (int i = 0; i < compilePath.size(); i++) {
+            String element = compilePath.get(i);
+            runtimeUrls[i] = new File(element).toURI().toURL();
+        }
+        return new URLClassLoader(runtimeUrls, Thread.currentThread().getContextClassLoader());
     }
 }

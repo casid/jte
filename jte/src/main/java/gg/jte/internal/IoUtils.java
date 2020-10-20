13589cc854f490bf91bbbd26658b0563033f1481
Andreas Hager <andy@mazebert.com>
2020-10-20 06:15:01 +0200

    Remove unused imports

--- jte/src/main/java/gg/jte/internal/IoUtils.java
+++ jte/src/main/java/gg/jte/internal/IoUtils.java
@@ -5,8 +5,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
 
 public final class IoUtils {
 

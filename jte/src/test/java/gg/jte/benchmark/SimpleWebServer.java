7aabbd2ca8c497653b37e6f9a286cf47742b76e9
Andreas Hager <andy@mazebert.com>
2020-12-21 08:47:04 +0100

    Simplify example

--- jte/src/test/java/gg/jte/benchmark/SimpleWebServer.java
+++ jte/src/test/java/gg/jte/benchmark/SimpleWebServer.java
@@ -29,7 +29,7 @@ public class SimpleWebServer implements HttpHandler {
 
     private void start() throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/", new SimpleWebServer());
+        server.createContext("/", this);
         server.start();
 
         System.out.println("Server started.");

d11af2fe22b976fa88fd235baa32e506b16b1b96
Andreas Hager <andy@mazebert.com>
2021-03-31 15:44:48 +0200

    Use binary static content for SimpleWebServer example

--- jte/src/test/java/gg/jte/benchmark/SimpleWebServer.java
+++ jte/src/test/java/gg/jte/benchmark/SimpleWebServer.java
@@ -5,15 +5,13 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
-import gg.jte.output.StringOutput;
-import gg.jte.output.StringOutputPool;
+import gg.jte.output.Utf8ByteOutput;
 import gg.jte.resolve.DirectoryCodeResolver;
 
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -23,9 +21,14 @@ public class SimpleWebServer implements HttpHandler {
     }
 
     private final DirectoryCodeResolver codeResolver = new DirectoryCodeResolver(Paths.get("jte", "src", "test", "resources", "benchmark"));
-    private final TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
-    private final StringOutputPool stringOutputPool = new StringOutputPool();
+    private final TemplateEngine templateEngine = createTemplateEngine();
     private final AtomicInteger visits = new AtomicInteger();
+
+    private TemplateEngine createTemplateEngine() {
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        templateEngine.setBinaryStaticContent(true);
+        return templateEngine;
+    }
 
     private void start() throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
@@ -37,14 +40,12 @@ public class SimpleWebServer implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        StringOutput output = stringOutputPool.get();
+        Utf8ByteOutput output = new Utf8ByteOutput();
         templateEngine.render("welcome.jte", new WelcomePage(visits.incrementAndGet()), output);
 
-        byte[] bytes = output.toString().getBytes(StandardCharsets.UTF_8);
-
-        exchange.sendResponseHeaders(200, bytes.length);
+        exchange.sendResponseHeaders(200, output.getContentLength());
         try (OutputStream os = new BufferedOutputStream(exchange.getResponseBody())) {
-            os.write(bytes);
+            output.writeTo(os);
         }
     }
 }

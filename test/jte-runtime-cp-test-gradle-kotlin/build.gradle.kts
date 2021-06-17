7f7779c1401f00620768207279a0544cb1128c6e
casid <andy@mazebert.com>
2021-06-17 20:04:03 +0200

    Bump version to 1.11.1-SNAPSHOT for further development

--- test/jte-runtime-cp-test-gradle-kotlin/build.gradle.kts
+++ test/jte-runtime-cp-test-gradle-kotlin/build.gradle.kts
@@ -3,7 +3,7 @@ import java.nio.file.Paths
 
 plugins {
     kotlin("jvm") version "1.4.30"
-    id("gg.jte.gradle") version("1.11.0")
+    id("gg.jte.gradle") version("1.11.1-SNAPSHOT")
 }
 
 repositories {
@@ -14,7 +14,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.junit.jupiter:junit-jupiter:5.4.2")
-    implementation("gg.jte:jte-runtime:1.11.0")
+    implementation("gg.jte:jte-runtime:1.11.1-SNAPSHOT")
 }
 
 tasks.test {

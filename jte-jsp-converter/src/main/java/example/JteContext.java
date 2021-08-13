d9f34196d9ae1c388fe229b3f30943efed36925e
casid <andy@mazebert.com>
2021-08-13 15:24:13 +0200

    Bump version to 1.11.2

--- jte-jsp-converter/src/main/java/example/JteContext.java
+++ jte-jsp-converter/src/main/java/example/JteContext.java
@@ -13,6 +13,7 @@ public class JteContext {
     /**
      * Taken from org.apache.el.parser.AstEmpty implementation
      * @param obj the object to check for JSP like emptiness
+     * @return true if empty
      */
     public static boolean isEmpty(Object obj) {
         if (obj == null) {
@@ -41,6 +42,7 @@ public class JteContext {
      * Dummy localization method. Usually {@link gg.jte.support.LocalizationSupport} in a thread local will help you implement this method.
      * @param key the localization key
      * @param params parameters for
+     * @return a localized content
      */
     public static Content localize(String key, Object ... params) {
         return null;

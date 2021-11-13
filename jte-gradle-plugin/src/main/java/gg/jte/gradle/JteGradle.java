9f440844a2ea4e511e85ac7434ea2e808868a07a
casid <andy@mazebert.com>
2021-11-13 06:15:49 +0100

    Apply project formatting to JteGradle

--- jte-gradle-plugin/src/main/java/gg/jte/gradle/JteGradle.java
+++ jte-gradle-plugin/src/main/java/gg/jte/gradle/JteGradle.java
@@ -8,19 +8,17 @@ import org.gradle.api.UnknownTaskException;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.SourceSet;
-import org.gradle.api.tasks.SourceSetContainer;
 import org.gradle.api.tasks.TaskProvider;
 
 import java.io.File;
 
-@SuppressWarnings("UnstableApiUsage")
 public class JteGradle implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         project.getPlugins().apply(JavaPlugin.class);
-        SourceSet main = _getMainSourceSet(project);
+        SourceSet main = getMainSourceSet(project);
         JteExtension extension = project.getExtensions().create("jte", JteExtension.class);
-        _defaults(project, extension, main);
+        defaults(project, extension, main);
 
         TaskProvider<PrecompileJteTask> precompileJteTask = project.getTasks().register("precompileJte", PrecompileJteTask.class, extension);
         precompileJteTask.configure(t -> t.dependsOn("compileJava"));
@@ -28,44 +26,34 @@ public class JteGradle implements Plugin<Project> {
 
         TaskProvider<GenerateJteTask> generateJteTask = project.getTasks().register("generateJte", GenerateJteTask.class, extension);
         project.getTasks().named("compileJava").configure(t -> t.dependsOn(generateJteTask));
-        try
-        {
+        try {
             project.getTasks().named("compileKotlin").configure(t -> t.dependsOn(generateJteTask));
+        } catch (UnknownTaskException ignore) {
         }
-        catch (UnknownTaskException ignore)
-        {}
 
         project.getTasks().named("clean").configure(t -> t.dependsOn("cleanPrecompileJte", "cleanGenerateJte")); // clean tasks are generated based on task outputs
 
         generateJteTask.configure(t -> {
-            if (extension.getStage().isPresent() && extension.getStage().get() == JteStage.GENERATE)
-            {
+            if (extension.getStage().isPresent() && extension.getStage().get() == JteStage.GENERATE) {
                 main.getJava().srcDir(t.getTargetDirectory());
                 main.getResources().srcDir(t.getTargetResourceDirectory());
             }
         });
     }
 
-    private SourceSet _getMainSourceSet(Project project)
-    {
+    private SourceSet getMainSourceSet(Project project) {
         JavaPluginConvention javaPluginConvention = project.getConvention().getPlugin(JavaPluginConvention.class);
         return javaPluginConvention.getSourceSets().findByName("main");
     }
 
-    private void _defaults(Project project, JteExtension extension, SourceSet main)
-    {
+    private void defaults(Project project, JteExtension extension, SourceSet main) {
         extension.getSourceDirectory().convention(project.file("src/main/jte").toPath()); // TODO can it use sourceset?
         extension.getTargetDirectory().convention(extension.getStage().map(stage -> {
-            if (stage == JteStage.PRECOMPILE)
-            {
+            if (stage == JteStage.PRECOMPILE) {
                 return project.file("jte-classes").toPath();
-            }
-            else if (stage == JteStage.GENERATE)
-            {
+            } else if (stage == JteStage.GENERATE) {
                 return new File(project.getBuildDir(), "generated-sources/jte").toPath();
-            }
-            else
-            {
+            } else {
                 //noinspection ConstantConditions // according to https://docs.gradle.org/current/javadoc/org/gradle/api/provider/Provider.html#map-org.gradle.api.Transformer- it's ok to return null here
                 return null;
             }

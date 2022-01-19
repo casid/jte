8cc8b67871bd9b9b5fbc46b8bd3cd799172bc202
Andreas Hager <andy@mazebert.com>
2022-01-19 09:20:20 +0100

    Bump version to 2.0.0-SNAPSHOT

--- test/jte-runtime-cp-test-gradle-kotlin/build.gradle.kts
+++ test/jte-runtime-cp-test-gradle-kotlin/build.gradle.kts
@@ -3,7 +3,7 @@ import java.nio.file.Paths
 
 plugins {
     kotlin("jvm") version "1.5.20"
-    id("gg.jte.gradle") version("1.12.2-SNAPSHOT")
+    id("gg.jte.gradle") version("2.0.0-SNAPSHOT")
 }
 
 repositories {
@@ -14,7 +14,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.junit.jupiter:junit-jupiter:5.4.2")
-    implementation("gg.jte:jte-runtime:1.12.2-SNAPSHOT")
+    implementation("gg.jte:jte-runtime:2.0.0-SNAPSHOT")
 }
 
 tasks.test {

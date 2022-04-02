8db43c7fb50936d9f53d3e61c9ec6ac634d2066b
Frederik Hahne <atomfrede@users.noreply.github.com>
2022-04-02 07:42:00 +0200

    initial spring boot starter implementation (#144)
    * initial spring boot starter implementation
    
    * add readme for boot starter usage
    
    * add note about webmvc and webflux
    
    * make it compatible with jdk8 and up

---
+++ jte-spring-boot-starter/src/main/java/gg/jte/springframework/boot/autoconfigure/JteProperties.java
@@ -0,0 +1,34 @@
+package gg.jte.springframework.boot.autoconfigure;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.env.Environment;
+import org.springframework.lang.NonNull;
+
+import java.util.Arrays;
+
+@ConfigurationProperties(prefix = "gg.jte")
+public class JteProperties {
+
+    private String productionProfileName = "prod";
+    private String templateLocation = "src/main/jte";
+
+    public String getProductionProfileName() {
+        return productionProfileName;
+    }
+
+    public void setProductionProfileName(String productionProfileName) {
+        this.productionProfileName = productionProfileName;
+    }
+
+    public String getTemplateLocation() {
+        return templateLocation;
+    }
+
+    public void setTemplateLocation(String templateLocation) {
+        this.templateLocation = templateLocation;
+    }
+
+    public boolean isProductionEnabled(@NonNull Environment environment) {
+        return Arrays.stream(environment.getActiveProfiles()).anyMatch(it -> it.equals(this.getProductionProfileName()));
+    }
+}
---
+++ jte-spring-boot-starter/src/main/java/gg/jte/springframework/boot/autoconfigure/ReactiveJteView.java
@@ -0,0 +1,55 @@
+package gg.jte.springframework.boot.autoconfigure;
+
+import gg.jte.TemplateEngine;
+import gg.jte.output.Utf8ByteOutput;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.core.io.buffer.PooledDataBuffer;
+import org.springframework.http.MediaType;
+import org.springframework.lang.NonNull;
+import org.springframework.web.reactive.result.view.AbstractUrlBasedView;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Map;
+
+public class ReactiveJteView extends AbstractUrlBasedView {
+    private final TemplateEngine templateEngine;
+
+    public ReactiveJteView(TemplateEngine templateEngine) {
+        this.templateEngine = templateEngine;
+    }
+
+    @Override
+    public boolean checkResourceExists(Locale locale) {
+        return templateEngine.hasTemplate(this.getUrl());
+    }
+
+    @Override
+    @NonNull
+    protected Mono<Void> renderInternal(@NonNull Map<String, Object> renderAttributes, MediaType contentType, ServerWebExchange exchange) {
+
+        return exchange.getResponse().writeWith(Mono
+                .fromCallable(() -> {
+                    String url = this.getUrl();
+                    Utf8ByteOutput output = new Utf8ByteOutput();
+                    templateEngine.render(url, renderAttributes, output);
+
+                    DataBuffer dataBuffer = exchange.getResponse().bufferFactory().allocateBuffer();
+                    try {
+                        output.writeTo(dataBuffer.asOutputStream());
+                        return dataBuffer;
+                    } catch (IOException ex){
+                        DataBufferUtils.release(dataBuffer);
+                        String message = "Could not load JTE template for URL [" + getUrl() + "]";
+                        throw new IllegalStateException(message, ex);
+                    }catch (Throwable ex) {
+                        DataBufferUtils.release(dataBuffer);
+                        throw ex;
+                    }}).doOnDiscard(PooledDataBuffer.class, DataBufferUtils::release));
+
+    }
+
+}

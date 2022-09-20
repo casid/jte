1b6f457ad552106a36765577d9a138f74d89304c
casid <andy@mazebert.com>
2022-09-20 20:45:30 +0200

    Bump version to 2.2.1-SNAPSHOT for further development

--- test/jte-runtime-cp-test-gradle-kotlin/build.gradle.kts
+++ test/jte-runtime-cp-test-gradle-kotlin/build.gradle.kts
@@ -3,7 +3,7 @@ import java.nio.file.Paths
 
 plugins {
     kotlin("jvm") version "1.7.10"
-    id("gg.jte.gradle") version("2.2.0")
+    id("gg.jte.gradle") version("2.2.1-SNAPSHOT")
 }
 
 repositories {
@@ -14,7 +14,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.junit.jupiter:junit-jupiter:5.4.2")
-    implementation("gg.jte:jte-runtime:2.2.0")
+    implementation("gg.jte:jte-runtime:2.2.1-SNAPSHOT")
 }
 
 tasks.test {

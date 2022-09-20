e89a6aaaa25701441b5fc9c052274b57e8217251
casid <andy@mazebert.com>
2022-09-20 20:21:15 +0200

    Bump version to 2.2.0

--- test/jte-runtime-cp-test-gradle-kotlin-convention/build.gradle.kts
+++ test/jte-runtime-cp-test-gradle-kotlin-convention/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
-    id("gg.jte.gradle") version("2.1.3-SNAPSHOT")
+    id("gg.jte.gradle") version("2.2.0")
 }
 
 repositories {
@@ -13,7 +13,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.junit.jupiter:junit-jupiter:5.4.2")
-    implementation("gg.jte:jte-runtime:2.1.3-SNAPSHOT")
+    implementation("gg.jte:jte-runtime:2.2.0")
 }
 
 jte {
--- test/jte-runtime-cp-test-gradle-kotlin/build.gradle.kts
+++ test/jte-runtime-cp-test-gradle-kotlin/build.gradle.kts
@@ -3,7 +3,7 @@ import java.nio.file.Paths
 
 plugins {
     kotlin("jvm") version "1.7.10"
-    id("gg.jte.gradle") version("2.1.3-SNAPSHOT")
+    id("gg.jte.gradle") version("2.2.0")
 }
 
 repositories {
@@ -14,7 +14,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.junit.jupiter:junit-jupiter:5.4.2")
-    implementation("gg.jte:jte-runtime:2.1.3-SNAPSHOT")
+    implementation("gg.jte:jte-runtime:2.2.0")
 }
 
 tasks.test {

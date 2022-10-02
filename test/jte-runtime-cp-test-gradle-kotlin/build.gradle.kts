034d945a3959a2561a14af6884231fdfc2e5852e
Andreas Hager <andy@mazebert.com>
2022-10-02 14:31:33 +0200

    #177 bump kotlin version to 1.7.20

--- test/jte-runtime-cp-test-gradle-kotlin/build.gradle.kts
+++ test/jte-runtime-cp-test-gradle-kotlin/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.nio.file.Paths
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
     id("gg.jte.gradle") version("2.2.1-SNAPSHOT")
 }
 

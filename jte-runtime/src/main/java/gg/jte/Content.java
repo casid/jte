a699cba292a21ecd297c9ff7476cd3ce2c294767
Andreas Hager <andy@mazebert.com>
2022-11-19 06:29:22 +0100

    Add javadoc for gg.jte.Content

--- jte-runtime/src/main/java/gg/jte/Content.java
+++ jte-runtime/src/main/java/gg/jte/Content.java
@@ -1,5 +1,45 @@
 package gg.jte;
 
+/**
+ * This interface can be used to create reusable content blocks for templates through Java code.
+ *
+ * When using the content block shorthand <code>@`...`</code> in templates, behind the scenes an anonymous class of
+ * {@link Content} is created as well. For example:
+ * <pre>
+ *     !{var name = "world";}
+ *     !{var myContent = @`
+ *         Hello ${name}!
+ *         This is a reusable content block.
+ *     `;}
+ *
+ *     ${myContent}
+ *     ${myContent}
+ * </pre>
+ *
+ * Would be the same as:
+ * <pre>
+ *     public class MyContent implements Content {
+ *         private final String name;
+ *
+ *         public MyContent(String name) {
+ *             this.name = name;
+ *         }
+ *
+ *         &#64;Override
+ *         public void writeTo(TemplateOutput output) {
+ *             output.writeContent("Hello ");
+ *
+ *             // User provided content, must be output escaped!
+ *             output.writeUserContent(name);
+ *
+ *             output.writeContent("!\n This is a reusable content block.");
+ *         }
+ *     }
+ * </pre>
+ *
+ * While the above example doesn't make a lot of sense, {@link Content} can be a very powerful
+ * abstraction for complex tasks.
+ */
 public interface Content {
     void writeTo(TemplateOutput output);
 

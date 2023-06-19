744fb6d8c04a6c673cfbd03233e001c0f9bc2a4c
casid <andy@mazebert.com>
2023-06-19 21:19:55 +0200

    Bump version to 3.0.1-SNAPSHOT for further development

--- test/jte-runtime-test-gradle-kotlin-convention/build.gradle.kts
+++ test/jte-runtime-test-gradle-kotlin-convention/build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
     id("java")
-    id("gg.jte.gradle").version("3.0.0")
+    id("gg.jte.gradle").version("3.0.1-SNAPSHOT")
 }
 
 repositories {
@@ -14,7 +14,7 @@ tasks.test {
 }
 
 dependencies {
-    implementation("gg.jte:jte-runtime:3.0.0")
+    implementation("gg.jte:jte-runtime:3.0.1-SNAPSHOT")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     testImplementation("org.assertj:assertj-core:3.23.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")

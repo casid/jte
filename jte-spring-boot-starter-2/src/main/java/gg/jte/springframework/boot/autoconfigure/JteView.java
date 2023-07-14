ebb8f5e6e0a81ab88c65b715d00ab37b8a0f8739
Thomas Schuehly <thomas.schuehly@outlook.com>
2023-07-14 18:22:46 +0200

    Align JteView

--- jte-spring-boot-starter-2/src/main/java/gg/jte/springframework/boot/autoconfigure/JteView.java
+++ jte-spring-boot-starter-2/src/main/java/gg/jte/springframework/boot/autoconfigure/JteView.java
@@ -1,7 +1,7 @@
 package gg.jte.springframework.boot.autoconfigure;
 
 import gg.jte.TemplateEngine;
-import gg.jte.output.Utf8ByteOutput;
+import gg.jte.output.PrintWriterOutput;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.view.AbstractTemplateView;
 
@@ -24,16 +24,10 @@ public class JteView extends AbstractTemplateView {
 
     @Override
     protected void renderMergedTemplateModel(Map<String, Object> model, javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws Exception {
-
         String url = this.getUrl();
-
-        Utf8ByteOutput output = new Utf8ByteOutput();
-        templateEngine.render(url, model, output);
-
         response.setContentType(MediaType.TEXT_HTML_VALUE);
         response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        response.setContentLength(output.getContentLength());
-
-        output.writeTo(response.getOutputStream());
+        PrintWriterOutput output = new PrintWriterOutput(response.getWriter());
+        templateEngine.render(url, model, output);
     }
 }

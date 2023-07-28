b969aa82fb882ea0bd86bd5b24a65c3414018b43
casid <andy@mazebert.com>
2023-07-28 18:29:29 +0200

    Bump version to 3.0.2

--- test/jte-runtime-cp-test-gradle-kotlin-convention/build.gradle.kts
+++ test/jte-runtime-cp-test-gradle-kotlin-convention/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.10"
-    id("gg.jte.gradle") version("3.0.2-SNAPSHOT")
+    id("gg.jte.gradle") version("3.0.2")
 }
 
 repositories {
@@ -13,7 +13,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.junit.jupiter:junit-jupiter:5.4.2")
-    implementation("gg.jte:jte-runtime:3.0.2-SNAPSHOT")
+    implementation("gg.jte:jte-runtime:3.0.2")
 }
 
 jte {

9edd8c9266334062cddac6f0ea28f654bfc2f86c
Andreas Hager <andy@mazebert.com>
2023-09-30 06:41:00 +0200

    #278 add more module test cases

--- jte/src/test/java/gg/jte/TemplateEngine_ModulesTest.java
+++ jte/src/test/java/gg/jte/TemplateEngine_ModulesTest.java
@@ -1,6 +1,7 @@
 package gg.jte;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 import java.nio.file.Path;
 
@@ -24,14 +25,49 @@ public class TemplateEngine_ModulesTest {
    }
 
    @Test
-   void threeModules() {
-      DirectoryCodeResolver codeResolver = new DirectoryCodeResolver(Path.of("src/test/modules/three-modules/app"));
+   void twoModulesCycle() {
+      DirectoryCodeResolver codeResolver = new DirectoryCodeResolver(Path.of("src/test/modules/two-modules-cycle/app"));
+      TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+
+      Throwable throwable = catchThrowable(() -> templateEngine.render("page.jte", null, new StringOutput()));
+
+      assertThat(throwable).isInstanceOf(StackOverflowError.class); // TODO better error message :-D
+   }
+
+   @Test
+   void threeModulesDifferentAlias() {
+      DirectoryCodeResolver codeResolver = new DirectoryCodeResolver(Path.of("src/test/modules/three-modules-different-alias/app"));
       TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
 
       StringOutput output = new StringOutput();
       templateEngine.render("page.jte", null, output);
 
       assertThat(output.toString().trim()).isEqualToNormalizingNewlines("<p>line chart (app)</p>\nline chart (core)");
+   }
+
+   @Test
+   void threeModulesSameAlias() {
+      DirectoryCodeResolver codeResolver = new DirectoryCodeResolver(Path.of("src/test/modules/three-modules-same-alias/app"));
+      TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+
+      StringOutput output = new StringOutput();
+      templateEngine.render("page.jte", null, output);
+
+      assertThat(output.toString().trim()).isEqualToNormalizingNewlines("<p>line chart (app)</p>\nline chart (core)");
+   }
+
+   @Test
+   void emptyTopLevelModule() {
+      DirectoryCodeResolver codeResolver = new DirectoryCodeResolver(Path.of("src/test/modules/empty-top-level-module"));
+      TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+
+      StringOutput output = new StringOutput();
+      templateEngine.render("checkout/page.jte", null, output);
+
+      assertThat(output.toString().trim()).isEqualToNormalizingNewlines("""
+              <style>important</style>
+              <p>line chart (checkout)</p>
+              line chart (core)""");
    }
 
    // TODO adjust precompileAll() and generateAll() to iterate over all module files as well!

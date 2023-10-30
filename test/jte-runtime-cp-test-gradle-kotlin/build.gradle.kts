cbd706c2262ad159972d4be19eba620ecc0c9c21
casid <andy@mazebert.com>
2023-10-30 20:20:55 +0100

    Bump version to 3.1.4

--- test/jte-runtime-cp-test-gradle-kotlin/build.gradle.kts
+++ test/jte-runtime-cp-test-gradle-kotlin/build.gradle.kts
@@ -3,7 +3,7 @@ import java.nio.file.Paths
 
 plugins {
     kotlin("jvm") version "1.9.10"
-    id("gg.jte.gradle") version("3.1.4-SNAPSHOT")
+    id("gg.jte.gradle") version("3.1.4")
 }
 
 repositories {
@@ -14,7 +14,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.junit.jupiter:junit-jupiter:5.4.2")
-    implementation("gg.jte:jte-runtime:3.1.4-SNAPSHOT")
+    implementation("gg.jte:jte-runtime:3.1.4")
 }
 
 tasks.test {

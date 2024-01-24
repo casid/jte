8782843cc9f5562228138ad3d86deb03082721a5
Marius Volkhart <marius@volkhart.com>
2024-01-24 15:31:04 -0500

    fixup! Reformat the templates for Kotlin model code
    Add import for Collectors

--- jte-models/src/test/java/gg/jte/models/generator/TestModelExtension.java
+++ jte-models/src/test/java/gg/jte/models/generator/TestModelExtension.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static gg.jte.extension.api.mocks.MockConfig.mockConfig;
 import static gg.jte.extension.api.mocks.MockParamDescription.mockParamDescription;

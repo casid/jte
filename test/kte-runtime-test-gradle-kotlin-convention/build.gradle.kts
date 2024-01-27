6500412efc44edd52360717e6ebd882d799d4436
casid <andy@mazebert.com>
2024-01-27 05:50:11 +0100

    Bump version to 3.1.9-SNAPSHOT for further development

--- test/kte-runtime-test-gradle-kotlin-convention/build.gradle.kts
+++ test/kte-runtime-test-gradle-kotlin-convention/build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
     kotlin("jvm") version "1.9.10"
-    id("gg.jte.gradle") version("3.1.8")
+    id("gg.jte.gradle") version("3.1.9-SNAPSHOT")
 }
 
 repositories {
@@ -16,7 +16,7 @@ tasks.test {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.junit.jupiter:junit-jupiter:5.9.2")
-    implementation("gg.jte:jte-runtime:3.1.8")
+    implementation("gg.jte:jte-runtime:3.1.9-SNAPSHOT")
 }
 
 jte {

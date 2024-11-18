1ea506077d9f0858886f1a2ecb91857462418e09
casid <andy@mazebert.com>
2024-11-18 12:57:30 +0100

    Revert "Add template change watcher for livereload (#398)"
    This reverts commit 84954def9d8fadc652c1fca9ef851906658f62ea.

--- jte-spring-boot-starter-3/src/main/java/gg/jte/springframework/boot/autoconfigure/JteAutoConfiguration.java
+++ jte-spring-boot-starter-3/src/main/java/gg/jte/springframework/boot/autoconfigure/JteAutoConfiguration.java
@@ -4,18 +4,11 @@ import gg.jte.CodeResolver;
 import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
 import gg.jte.resolve.DirectoryCodeResolver;
-import java.io.File;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.devtools.filewatch.FileSystemWatcher;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.web.servlet.view.AbstractTemplateViewResolver;
 
 import java.nio.file.FileSystems;
@@ -30,9 +23,6 @@ public class JteAutoConfiguration {
     public JteAutoConfiguration(JteProperties jteProperties) {
         this.jteProperties = jteProperties;
     }
-
-
-    Logger logger = LoggerFactory.getLogger(JteAutoConfiguration.class);
 
     @Bean
     @ConditionalOnMissingBean(JteViewResolver.class)
@@ -58,22 +48,5 @@ public class JteAutoConfiguration {
             return TemplateEngine.create(codeResolver, Paths.get("jte-classes"), ContentType.Html, getClass().getClassLoader());
         }
         throw new JteConfigurationException("You need to either set gg.jte.usePrecompiledTemplates or gg.jte.developmentMode to true ");
-    }
-
-    @Bean
-    @ConditionalOnProperty(name = "gg.jte.developmentMode", havingValue = "true")
-    FileSystemWatcher viewComponentFileSystemWatcher(ApplicationContext applicationContext, JteProperties jteProperties) {
-        var fileSystemWatcher = new FileSystemWatcher();
-        File jteDir = new File(jteProperties.getTemplateLocation());
-        fileSystemWatcher.addSourceDirectory(jteDir);
-        logger.info("Watching for template changes at: {}", jteDir.getAbsoluteFile().getPath());
-        fileSystemWatcher.addListener(
-            changeSet -> {
-                logger.debug("Detected change to template: {}", changeSet);
-                applicationContext.publishEvent(new ContextRefreshedEvent(applicationContext));
-            }
-        );
-        fileSystemWatcher.start();
-        return fileSystemWatcher;
     }
 }

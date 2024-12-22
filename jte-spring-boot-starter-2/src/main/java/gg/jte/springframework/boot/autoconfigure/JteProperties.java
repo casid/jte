43d7c2147f121d6800d538abc495315992233f06
Aleksey Genus <agenus@vivid.money>
2024-12-22 19:20:00 +0100

    Use spring-configuration-metadata.json generated by spring boot configuration processor instead of manually creating it

--- jte-spring-boot-starter-2/src/main/java/gg/jte/springframework/boot/autoconfigure/JteProperties.java
+++ jte-spring-boot-starter-2/src/main/java/gg/jte/springframework/boot/autoconfigure/JteProperties.java
@@ -4,9 +4,25 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "gg.jte")
 public class JteProperties {
+    /**
+     * You can precompile the templates for faster startup and rendering. To use this you need to include
+     * the Maven or Gradle Plugin: <a href="https://jte.gg/pre-compiling/">https://jte.gg/pre-compiling/</a>
+     */
     private Boolean usePrecompiledTemplates = false;
+
+    /**
+     * You can change the location where jte expects the templates to compile
+     */
     private String templateLocation = "src/main/jte";
+
+    /**
+     * You can configure the file suffix of jte templates the compiler resolves
+     */
     private String templateSuffix = ".jte";
+
+    /**
+     * Set whether all request attributes should be added to the model prior to merging with the template
+     */
     private boolean exposeRequestAttributes = false;
 
     public String getTemplateSuffix() {
--- jte-spring-boot-starter-3/src/main/java/gg/jte/springframework/boot/autoconfigure/JteProperties.java
+++ jte-spring-boot-starter-3/src/main/java/gg/jte/springframework/boot/autoconfigure/JteProperties.java
@@ -4,10 +4,31 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "gg.jte")
 public class JteProperties {
+
+    /**
+     * You can precompile the templates for faster startup and rendering. To use this you need to include
+     * the Maven or Gradle Plugin: <a href="https://jte.gg/pre-compiling/">https://jte.gg/pre-compiling/</a>
+     */
     private boolean usePrecompiledTemplates = false;
+
+    /**
+     * You can enable development mode so that the jte file watcher will watch for changes in templates and recompile them.
+     */
     private boolean developmentMode = false;
+
+    /**
+     * You can change the location where jte expects the templates to compile
+     */
     private String templateLocation = "src/main/jte";
+
+    /**
+     * You can configure the file suffix of jte templates the compiler resolves
+     */
     private String templateSuffix = ".jte";
+
+    /**
+     * Set whether all request attributes should be added to the model prior to merging with the template
+     */
     private boolean exposeRequestAttributes = false;
 
     public String getTemplateSuffix() {

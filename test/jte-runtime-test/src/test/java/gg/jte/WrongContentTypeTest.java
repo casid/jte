c32c626890473c338950186a6d40513876840928
iTob191 <itob.dev@gmail.com>
2026-03-17 09:28:47 +0100

    cleanup

--- test/jte-runtime-test/src/test/java/gg/jte/WrongContentTypeTest.java
+++ test/jte-runtime-test/src/test/java/gg/jte/WrongContentTypeTest.java
@@ -1,6 +1,7 @@
 package gg.jte;
 
 import gg.jte.output.StringOutput;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,6 +31,11 @@ public class WrongContentTypeTest {
     @BeforeEach
     void setUp() {
         model.hello = "Hello";
+    }
+
+    @AfterEach
+    void cleanUp() {
+        output.reset();
     }
 
     @Test
